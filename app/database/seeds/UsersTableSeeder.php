b088a35b8b72b64656ccc8e9df3200c93eb0d6b5
AlexanderZon <alexisanderson@ovi.com>
2015-01-09 13:03:31 -0430

    seed fixes

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -8,25 +8,14 @@ class UsersTableSeeder extends Seeder {
 	public function run()
 	{
 		
-		Users::create( array(
+		User::create( array(
 			'first_name' => 'Alexis',
 			'last_name' => 'Montenegro',
 			'username' => 'AlexanderZon',
 			'displayname' => 'Alexis Montenegro',
 			'email' => 'amontenegro@gmaicmedia.com.ve',
 			'password' => Hash::make('alexis23498535'),
-			'id_role' => '1',
-			'status' => 'active'
-			));
-		
-		Users::create( array(
-			'first_name' => 'Antony',
-			'last_name' => 'Borges',
-			'username' => 'robertdacorte',
-			'displayname' => 'Antony Borges',
-			'email' => 'aborges@gmaicmedia.com.ve',
-			'password' => Hash::make('robert18554560'),
-			'id_role' => '1',
+			'id_role' => 1,
 			'status' => 'active'
 			));
 
